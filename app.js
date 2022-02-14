672f65d7b938017472756f5b69f5e0eef4bc1e6e
Jonatan Morales <moralesorellana170498@gmail.com>
2022-02-14 06:25:27 -0600

    archivo js de animaciones hover

---
+++ app.js
@@ -0,0 +1,19 @@
+const views = document.getElementById("views");
+const titulo = document.getElementById("titulo");
+const linkFooter = document.getElementById("link_a");
+
+/* Agregar el hover*/
+const AddHover = () => { 
+    views.classList.add("cont__view-hover");
+}
+
+/* Remover el hover*/
+const RemoveHover = () => {
+    views.classList.remove("cont__view-hover");
+}
+
+titulo.addEventListener("mouseenter", AddHover);
+titulo.addEventListener("mouseout", RemoveHover);
+
+linkFooter.addEventListener("mouseenter", AddHover);
+linkFooter.addEventListener("mouseout", RemoveHover);
